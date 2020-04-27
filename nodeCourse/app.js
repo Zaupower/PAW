76ae15a1c950d881d718e875a6864a4f87b63f4f
MARCELO ANTONIO SOARES CARVALHO <8160287@estg.ipp.pt>
2020-04-27 02:55:26 +0100

    Adiciona routes para update article/delete article(ajax script)

--- nodeCourse/app.js
+++ nodeCourse/app.js
@@ -35,7 +35,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+
+//Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
+
+//-------------------------------------ROUTES-------------------------------------//
+
+
+//------------TEST ROUTE------------//
+
+app.get('/test', function(req, res){
+    res.render('test',{
+
+    });
+});
+
+//------------TEST ROUTE------------//
+
 //Home Route ou set public folder
 app.get('/', function(req, res){
     Article.find({}, function(err, articles){
@@ -61,7 +77,7 @@ app.get('/', function(req, res){
 //aceder ao html ou pug "add_articles"
 app.get('/articles/add', function(req, res){
 
-    console.log('hello')
+    console.log('hello');
     //add_article referese ao nome do ficheiro html que se quer abrir
     res.render('add_article', {
         title:'Articles'
@@ -89,6 +105,71 @@ app.post('/articles/add', function(req, res){
         }
     });
 });
+
+//Get Single Article information link route
+app.get('/article/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('article', {
+            article:article
+        });
+    });
+});
+
+
+//Load Edit Form
+app.get('/article/edit/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('edit_article', {
+            title:'Edit Article',
+            article:article
+        });
+    });
+});
+
+//Edit Submit POST route
+app.post('/articles/edit/:id', function(req, res){
+/*Para editar o artigo cria-se um objecto vazio para onde vai
+  igualar o artigo editado que vem de edit_article.pug neste 
+  caso req
+*/
+//objeto vazio
+    let article = {};
+
+    article.title = req.body.title;
+    article.body = req.body.body;
+    article.author = req.body.author;
+
+//query ee o id do objeto
+    let query = {_id:req.params.id};
+
+/*Em Article classe geral e adicionado o id (query)e os
+valores novos do artigo(article) e e utilizada a funcao
+Article.update
+*/
+    Article.updateOne(query, article, function(err){
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+
+//Delete Article by js script in public/js/main.js
+
+app.delete('/article/:id', function(req, res){
+    let query = {_id:req.params.id};
+    Article.remove(query, function(err){
+        if(err){
+            console.log(err);
+        }
+        res.send('Success');
+    });
+});
+
+//-------------------------------------ROUTES-------------------------------------//
 
 //informacao do inicio do servidor node
 app.listen(3000, function(){
